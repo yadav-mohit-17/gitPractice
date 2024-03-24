8965ae2aeb62ebb1ed891148bfb7c3630386fbe1
Mohit Kumar <143256804+yadav-mohit-17@users.noreply.github.com>
2024-03-24 20:58:53 +0530

    Updated code

---
+++ src/test/java/com/app/git/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
